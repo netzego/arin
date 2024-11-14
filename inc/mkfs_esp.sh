02d7fc21483c80e6e233cd0f3987d3c2aa92c0ce
netzego <git@netzego.de>
2024-11-14 23:38:21 +0100

    src: fix: rm -F32 from mkfs.vfat call

--- inc/mkfs_esp.sh
+++ inc/mkfs_esp.sh
@@ -3,6 +3,6 @@
 function mkfs_esp() {
     if [[ "$(lsblk -rno FSTYPE "${ESP_PATH}")" != "vfat" ]]; then
         # WARNING
-        mkfs.vfat -F32 "${ESP_PATH}"
+        mkfs.vfat "${ESP_PATH}"
     fi
 }
