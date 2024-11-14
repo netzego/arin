59e4b2e66f082dca0d1023a572bac2c113862ccc
netzego <git@netzego.de>
2024-11-14 12:49:20 +0100

    !src: bootloader: switch to efibootmgr

--- inc/install_bootloader.sh
+++ inc/install_bootloader.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 function install_bootloader() {
-    declare -r rootfs="${1:-${MOUNTDIR}}"
+    # - this needs tyo run after umounting and close the root and luks parts
+    # - we have to enter the encryption password ...
+    # bootctl --image="${VOLUME}" --esp-path=/efi --install-source=image install
+
+    efibootmgr --create --disk "${VOLUME}" --part 1 --loader '\EFI\systemd\systemd-bootx64.efi' --label "Linux Boot Manager" --unicode
+
+    # declare -r rootfs="${1:-${MOUNTDIR}}"
 
     # bootctl --root="${rootfs}" --esp-path=/efi --install-source=image install
-    bootctl --root="${rootfs}" --esp-path=/efi install
 
-    systemd-nspawn -D "${rootfs}" bootctl status
+    # systemd-nspawn -D "${rootfs}" bootctl status
+
     # systemd-nspawn -D "${rootfs}" bootctl list
 }
