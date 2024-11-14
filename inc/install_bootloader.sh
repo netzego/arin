d580ce1e1ff967c41456d385c4aee750fecdb0a7
netzego <git@netzego.de>
2024-11-14 23:39:53 +0100

    !src: fix: change bootloader flow
    - enable systemd-nspawn -D and efibootmgr
    - works w/ qemu usb boot install

--- inc/install_bootloader.sh
+++ inc/install_bootloader.sh
@@ -5,11 +5,10 @@ function install_bootloader() {
     # - we have to enter the encryption password ...
     # bootctl --image="${VOLUME}" --esp-path=/efi --install-source=image install
 
+    declare -r rootfs="${1:-${MOUNTDIR}}"
+    bootctl --root="${rootfs}" --esp-path=/efi --install-source=image install
+
     efibootmgr --create --disk "${VOLUME}" --part 1 --loader '\EFI\systemd\systemd-bootx64.efi' --label "Linux Boot Manager" --unicode
-
-    # declare -r rootfs="${1:-${MOUNTDIR}}"
-    # bootctl --root="${rootfs}" --esp-path=/efi --install-source=image install
-
     # systemd-nspawn -D "${rootfs}" bootctl status
     # systemd-nspawn -D "${rootfs}" bootctl list
 }
