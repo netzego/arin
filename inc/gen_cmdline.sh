366e495e0a7e1e5016d4615999340d5cdab17d52
netzego <git@netzego.de>
2024-11-14 17:09:11 +0100

    !src: cmdline: add console=tty0 as core arg

--- inc/gen_cmdline.sh
+++ inc/gen_cmdline.sh
@@ -4,7 +4,7 @@
 function gen_cmdline() {
     declare -r luks_uuid="${1}"
     declare -r root_uuid="${2}"
-    declare -r cmdline="rd.luks.name=${luks_uuid}=rootfs root=UUID=${root_uuid} rootflags=noatime,compress=zstd rw ${CMDLINE_EXTRA}"
+    declare -r cmdline="rd.luks.name=${luks_uuid}=rootfs root=UUID=${root_uuid} rootflags=noatime,compress=zstd rw console=tty0 ${CMDLINE_EXTRA}"
 
     echo "${cmdline}" | tee "${MOUNTDIR}/etc/kernel/cmdline"
 }
