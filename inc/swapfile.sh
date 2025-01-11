6b7249c72c4242ede2ecf20f8af3a412fc8c9a4f
netzego <git@netzego.de>
2025-01-11 23:24:09 +0100

    src: add checks for swapfile and dir

--- inc/swapfile.sh
+++ inc/swapfile.sh
@@ -3,10 +3,21 @@
 function swapfile() {
     declare -r swapfile="${1:-${WORKDIR}/mnt/swap/swapfile}"
     declare -r size="${2:-${SIZE_SWAPFILE}}"
-    declare -r swapdir="$(dirname "${swapfile}")"
+    declare -r dir="$(dirname "${swapfile}")"
 
-    if [[ ! -d "${swapdir}" ]]; then
-        err 3 "\`${swapdir}' is not a directory"
+    # check if dirname of swapfile is a) mountpoint and b) a btrfs subvolume
+    if [[ ! -d "${dir}" ]]; then
+        err 30 "'${dir}' is not a directory"
+    fi
+
+    if ! mountpoint -q "${dir}"; then
+        err 31 "'${dir}' is not a mountpoint."
+    fi
+
+    # check if $dir is a btrfs subvol
+    # https://stackoverflow.com/a/32865333
+    if [[ "$(stat --format=%i ${dir})" -ne 256 ]]; then
+        err 32 "'${dir}' is not a btrfs subvolume."
     fi
 
     btrfs -v filesystem mkswapfile --size "${size}" "${swapfile}"
