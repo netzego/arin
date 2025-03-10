21cf72d7489644e9c1d13ae2471e999adc38f08a
netzego <git@netzego.de>
2025-03-10 22:19:46 +0100

    src: add cmdline options to wait for root

--- inc/gen_cmdline.sh
+++ inc/gen_cmdline.sh
@@ -4,7 +4,7 @@
 function gen_cmdline() {
     declare -r luks_uuid="${1}"
     declare -r root_uuid="${2}"
-    declare -r cmdline="rd.luks.name=${luks_uuid}=rootfs root=UUID=${root_uuid} rootflags=noatime,compress=zstd ro ${CMDLINE_EXTRA}"
+    declare -r cmdline="rd.luks.name=${luks_uuid}=rootfs root=UUID=${root_uuid} rootflags=noatime,compress=zstd ro ${CMDLINE_EXTRA} rd.luks.options=timeout=0 rootflags=x-systemd.device-timeout=0"
 
     echo "${cmdline}" | tee "${MOUNTDIR}/etc/kernel/cmdline"
 }
