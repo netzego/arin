c0e344b8ee1617f0c8311470d51a99fe10c4cd3b
netzego <git@netzego.de>
2025-03-10 20:44:04 +0100

    src: add commented xtrace

--- arin.sh
+++ arin.sh
@@ -4,6 +4,7 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+# set -o xtrace
 
 declare -gr SCRIPTNAME="$(basename "${BASH_ARGV0}")"
 declare -gr SCRIPTDIR="$(dirname "$(realpath "${BASH_ARGV0}")")"
