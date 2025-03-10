e27d9f02c16b0f48ae6b47a5305c8f18f8ae38fe
netzego <git@netzego.de>
2025-03-10 21:34:56 +0100

    src: rename arin.sshkey to arin.authorized_keys

--- inc/copy_sshkey.sh
+++ inc/copy_sshkey.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function copy_sshkey() {
-    declare -r pubkey="${1:-${SCRIPTDIR}/arin.sshkey}"
+    declare -r pubkey="${1:-${SCRIPTDIR}/arin.authorized_keys}"
     declare -r rootfs="${2:-${MOUNTDIR}}"
 
     if [[ -f "${pubkey}" ]]; then
