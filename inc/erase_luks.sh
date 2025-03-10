59c36a43448394053bc535d2ccf9def63f78465c
netzego <git@netzego.de>
2025-03-10 21:41:54 +0100

    src: bugfix: unused local var $lukspart

--- inc/erase_luks.sh
+++ inc/erase_luks.sh
@@ -14,7 +14,7 @@ function erase_luks() {
         --key-file /dev/urandom \
         --batch-mode \
         --sector-size 512 \
-        "${LUKS_PATH}" "${mapname}"
+        "${lukspart}" "${mapname}"
 
     set +o errexit
     dd if=/dev/zero of="/dev/mapper/${mapname}" status=progress bs=1M
