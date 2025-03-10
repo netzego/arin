76ed66d07a4150dc64bc23d98b4c6e26f0a58004
netzego <git@netzego.de>
2025-03-10 20:27:19 +0100

    src: cleanup vars; win vars and swap

--- inc/vars.sh
+++ inc/vars.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2155,SC2034
 
+declare -gr VERSION="0.0.0"
 declare -gr RNDNR="$(uuidgen -t | tr -d '-')"
 declare -gr WORKDIR="$(mktemp -d -p /tmp "${SCRIPTNAME}.workdir-XXXXXXXX")"
 declare -gr KEYFILE="arin.keyfile"
@@ -9,16 +10,17 @@ declare -gr MOUNTDIR="${WORKDIR}/mnt"
 
 declare -gr TYPECODE_ESP="EF00"
 declare -gr TYPECODE_LINUXROOT="8304"
-declare -gr TYPECODE_WINDATA="0700"
-declare -gr TYPECODE_WINRESCUE="2700"
-declare -gr TYPECODE_WINRESERVED="0C01"
+# declare -gr TYPECODE_WINDATA="0700" # TODO delete me
+# declare -gr TYPECODE_WINRESCUE="2700"
+# declare -gr TYPECODE_WINRESERVED="0C01"
 
 declare -gr SIZE_ESP="${ESP_SIZE:-128M}"
 declare -gr SIZE_LINUXROOT="${ROOT_SIZE:-0}"
-declare -gr SIZE_SWAPFILE="128M"
-declare -gr SIZE_WINDATA="64M"    # TODO change me
-declare -gr SIZE_WINRESCUE="128M" # TODO change me
-declare -gr SIZE_WINRESERVED="16M"
+# declare -gr SIZE_SWAPFILE="$(grep ^MemTotal /proc/meminfo | xargs | cut -d' ' -f2)k"
+declare -gr SIZE_SWAPFILE="128m"
+# declare -gr SIZE_WINDATA="64M"    # TODO delete me
+# declare -gr SIZE_WINRESCUE="128M" # TODO change me
+# declare -gr SIZE_WINRESERVED="16M"
 
 filearray "PACKAGES" "${SCRIPTDIR}/inc/core.packages" "${PWD}/${SCRIPTNAME}.packages"
 declare -gr BTRFS_MOUNT_OPTIONS="noatime,compress=zstd"
@@ -36,11 +38,13 @@ declare -gra BTRFS_SUBVOLUMES=(
     "@swap:/swap"
 )
 
+declare -gr BATCH="${BATCH:-no}"
 declare -gr WIPE="${WIPE:-no}"
-declare -gr CREATE_WIN="${CREATE_WIN:-no}"
+declare -gr RANDOMIZE_LINUXROOT="${RANDOMIZE_LINUXROOT:-yes}"
+# declare -gr CREATE_WIN="${CREATE_WIN:-no}"
 declare -gr LOCALE="${LOCALE:-en_US.UTF-8}"
 declare -gr TIMEZONE="${TIMEZONE:-Europe/Berlin}"
 declare -gr KEYMAP="${KEYMAP:-us}"
-declare -gr NODENAME="${NODENAME:-arin}"
+declare -gr NODENAME="${NODENAME:-arin}" # use
 declare -gr CMDLINE_EXTRA="${CMDLINE_EXTRA:-}"
 declare -gr DEFAULT_SHELL="${DEFAULT_SHELL:-/usr/bin/bash}"
