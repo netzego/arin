e879fee1d3d0a63694a8137f9110d3518aa3141e
netzego <git@netzego.de>
2025-04-17 13:04:08 +0200

    src: add TODO comment

--- inc/copy_sshkey.sh
+++ inc/copy_sshkey.sh
@@ -4,6 +4,7 @@ function copy_sshkey() {
     declare -r pubkey="${1:-${SCRIPTDIR}/arin.authorized_keys}"
     declare -r rootfs="${2:-${MOUNTDIR}}"
 
+    # TODO: set owner and file permissions
     if [[ -f "${pubkey}" ]]; then
         cp -v "${pubkey}" "${rootfs}/root/.ssh/authorized_keys"
     fi
