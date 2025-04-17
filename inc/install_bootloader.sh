c44c4a5562be60f47ed423ba584fe3388a1d15f5
netzego <git@netzego.de>
2025-04-17 16:52:36 +0200

    src!: cleanup install_bootloader function

--- inc/install_bootloader.sh
+++ inc/install_bootloader.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
 function install_bootloader() {
-    # - this needs tyo run after umounting and close the root and luks parts
-    # - we have to enter the encryption password ...
-    # bootctl --image="${VOLUME}" --esp-path=/efi --install-source=image install
-
+    # --no-variables
+    # --random-seed=no
+    # --install-source=image
     declare -r rootfs="${1:-${MOUNTDIR}}"
-    bootctl --root="${rootfs}" --esp-path=/efi --install-source=image install
-
-    efibootmgr --create --disk "${VOLUME}" --part 1 --loader '\EFI\systemd\systemd-bootx64.efi' --label "Linux Boot Manager" --unicode
-    # systemd-nspawn -D "${rootfs}" bootctl status
-    # systemd-nspawn -D "${rootfs}" bootctl list
+    bootctl --root="${rootfs}" --esp-path=/efi --install-source=image --random-seed=no install
+    bootctl --root="${rootfs}" status
+    bootctl --root="${rootfs}" list
 }
